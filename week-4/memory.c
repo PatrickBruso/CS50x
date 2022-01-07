88b0cb718091d1136b38ca9a9f52ff1980774c18
Patrick Bruso <pkbruso@gmail.com>
2022-01-07 23:21:00 +0000

    Fri, Jan 7, 2022, 11:21 PM +00:00

---
+++ week-4/memory.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int *x = malloc(3 * sizeof(int));  // obtain memory for three int and assign to int pointer x
+    x[0] = 72;
+    x[1] = 73;
+    x[2] = 33;
+}
