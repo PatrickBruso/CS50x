33f4db394a540acd31f68c9875ab2bde2be143b5
Patrick Bruso <pkbruso@gmail.com>
2022-01-07 23:15:02 +0000

    Fri, Jan 7, 2022, 11:15 PM +00:00

--- week-4/copy.c
+++ week-4/copy.c
@@ -2,15 +2,38 @@
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    string s = get_string("s: ");
+    char *s = get_string("s: ");
 
-    string t = s;  // Points t to the same location as s
+    // Create pointer to new string and allocate memory
+    char *t = malloc(strlen(s) + 1);
+    // Error check if malloc fails
+    if (t == NULL)
+    {
+        return 1;
+    }
 
-    t[0] = toupper(t[0]);
+    /**for (int i = 0, n = strlen(s) + 1; i < n; i++)
+    {
+        t[i] = s[i];
+    }**/
+
+    strcpy(t, s); // same as the for loop
+
+    // Check for characters in string
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
 
     printf("s: %s\n", s);
     printf("t: %s\n", t);
+
+    // Free allocated space for t (get_string does this automatically for s)
+    free(t);
+
+    return 0; // successful termination
 }
