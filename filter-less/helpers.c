f6f119be3ecfa5296d727ea9b7343f7fd3135cc8
Patrick Bruso <pkbruso@gmail.com>
2022-01-13 21:44:36 +0000

    Thu, Jan 13, 2022, 9:44 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -92,7 +92,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE copy[i][j] = image[i][j];
+            copy[i][j] = image[i][j];
         }
     }
 
