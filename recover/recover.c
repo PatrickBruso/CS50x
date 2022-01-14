f621243da5581c72350b44cf6cd79724f17827d0
Patrick Bruso <pkbruso@gmail.com>
2022-01-14 21:04:02 +0000

    Fri, Jan 14, 2022, 9:04 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     // Check command-line arguments
-    if (argc != 1)
+    if (argc != 2)
     {
         printf("Usage: ./recover IMAGE\n");
         return 1;
