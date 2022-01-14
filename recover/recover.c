4e499fd074bd417db8576683cb1b28d5e090ca65
Patrick Bruso <pkbruso@gmail.com>
2022-01-14 22:15:55 +0000

    Fri, Jan 14, 2022, 10:15 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,6 +21,7 @@ int main(int argc, char *argv[])
     const int BUFFER_SIZE = 512; // set block size to 512 bytes for efficiency in reading
     uint8_t buffer[BUFFER_SIZE]; // create an array of 512 bytes to read from raw file
     int file_index = 0; // set file index for naming JPEGs found
+    char jpeg_file[8]; // array for JPEG files recovered
 
     // Read 512 bytes from input file
     while (fread(&buffer, sizeof(uint8_t), BUFFER_SIZE, input) == BUFFER_SIZE)
@@ -28,7 +29,7 @@ int main(int argc, char *argv[])
         // Check first four bytes from raw file to see if they match JPEG "signature"
         if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0)) // Last conditional uses bitwise operator to check if first four bytes are "1110"
         {
-
+            sprintf(jpeg_file, "%03i.jpg", file_index); // 
         }
     }
 
