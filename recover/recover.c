112de9c19002b6dbfbf3f1ddfc858e2faa44702f
Patrick Bruso <pkbruso@gmail.com>
2022-01-14 23:36:15 +0000

    Fri, Jan 14, 2022, 11:36 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,15 +25,16 @@ int main(int argc, char *argv[])
     BYTE buffer[BUFFER_SIZE]; // create an array of 512 bytes to read from raw file
     int file_index = 0; // set file index for naming JPEGs found
     char jpeg_file[9]; // array for JPEG files recovered
-    FILE* output = NULL; // create output file to use
+    FILE *output = NULL; // create output file to use
 
     // Read 512 bytes from input file
     while (fread(&buffer, 1, BUFFER_SIZE, input) == BUFFER_SIZE)
     {
         // Check first four bytes from raw file to see if they match JPEG "signature"
-        if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0)) // Last conditional uses bitwise operator to check if first four bytes are "1110"
+        if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff)
+            && ((buffer[3] & 0xf0) == 0xe0)) // Last conditional uses bitwise operator to check if first four bytes are "1110"
         {
-            sprintf(jpeg_file, "%03i.jpg", file_index); // use sprintf function to print formatted string to memory using the file_index number (to three places).
+            sprintf(jpeg_file, "%03i.jpg", file_index); // use sprintf function to print formatted string to memory
             if (file_index == 0) // check for first JPEG file
             {
                 output = fopen(jpeg_file, "w"); // open file we just created
@@ -49,7 +50,7 @@ int main(int argc, char *argv[])
             file_index++; // increase the index by 1 for next file name
         }
         // Check if new header or continue reading after a JPEG has been created
-        else if(output != NULL)
+        else if (output != NULL)
         {
             fwrite(&buffer, BUFFER_SIZE, 1, output);
         }
