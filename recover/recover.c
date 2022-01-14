2455e9f06b265da2107236bccecb4cb83daa8b99
Patrick Bruso <pkbruso@gmail.com>
2022-01-14 22:17:14 +0000

    Fri, Jan 14, 2022, 10:17 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
         // Check first four bytes from raw file to see if they match JPEG "signature"
         if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0)) // Last conditional uses bitwise operator to check if first four bytes are "1110"
         {
-            sprintf(jpeg_file, "%03i.jpg", file_index); // 
+            sprintf(jpeg_file, "%03i.jpg", file_index); // use sprintf function to print formatted string to memory using the file_index number (to three places).
         }
     }
 
