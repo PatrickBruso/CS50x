2cd2f5046068610de27306bc9185b6b61833e6f8
Patrick Bruso <pkbruso@gmail.com>
2022-01-14 23:05:52 +0000

    Fri, Jan 14, 2022, 11:05 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 const int BUFFER_SIZE = 512; // set block size to 512 bytes for efficiency in reading
 typedef uint8_t BYTE;
@@ -26,7 +27,7 @@ int main(int argc, char *argv[])
     char jpeg_file[8]; // array for JPEG files recovered
 
     // Read 512 bytes from input file
-    while (fread(&buffer, BUFFER_SIZE, 1, input))
+    while (fread(&buffer, 1, BUFFER_SIZE, input) == BUFFER_SIZE)
     {
         // Check first four bytes from raw file to see if they match JPEG "signature"
         if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0)) // Last conditional uses bitwise operator to check if first four bytes are "1110"
@@ -34,10 +35,10 @@ int main(int argc, char *argv[])
             sprintf(jpeg_file, "%03i.jpg", file_index); // use sprintf function to print formatted string to memory using the file_index number (to three places).
             FILE *output = fopen(jpeg_file, "w");
             file_index++;
-            fwrite(&buffer, BUFFER_SIZE, 1, output);
+            fwrite(&buffer, 1, BUFFER_SIZE, output);
 
             // Check if new header or continue reading
-            fread(&buffer, BUFFER_SIZE, 1, input);
+            fread(&buffer, 1, BUFFER_SIZE, input);
             while ((buffer[0] != 0xff) && (buffer[1] != 0xd8) && (buffer[2] != 0xff) && ((buffer[3] & 0xf0) != 0xe0))
             {
                 fwrite(&buffer, BUFFER_SIZE, 1, output);
