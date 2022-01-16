b11dd29be93793dfe3b26136e569f7af9c246965
Patrick Bruso <pkbruso@gmail.com>
2022-01-16 21:49:47 +0000

    Sun, Jan 16, 2022, 9:49 PM +00:00

---
+++ week-5/linkedlist.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stlib.h>
+
+typdef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(void)
+{
+    // List of size
+    node *list = NULL;
+}
