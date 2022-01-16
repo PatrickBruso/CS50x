a518920dd3fdb1fee8107fb0c90f58e6164e1552
Patrick Bruso <pkbruso@gmail.com>
2022-01-16 02:48:43 +0000

    Sun, Jan 16, 2022, 2:48 AM +00:00

--- week-5/list.c
+++ week-5/list.c
@@ -16,21 +16,19 @@ int main(void)
     }
 
     // Put memory on heap
+    // Dynamically allocate an array of size 3
     int *list2 = malloc(3 * sizeof(int));
     if (list2 == NULL)
     {
         return 1;
     }
 
+    // Assign three numbers to that array
     list2[0] = 1;
     list2[1] = 2;
     list2[2] = 3;
 
-    // Can change size of array, unlike stack
-    //list2 = malloc(4 * sizeof(int));
-    //list2[3] = 4;
-
-    // Right way to do above (add to array)
+    // Allocate new array of size 4
     int *tmp = malloc(4 * sizeof(int));
     if (tmp == NULL)
     {
@@ -38,20 +36,25 @@ int main(void)
         return 1;
     }
 
+    // Copy numbers from old array into new array
     for (int i = 0; i < 3; i++)
     {
         tmp[i] = list2[i];
     }
+    // Add fourth number to new array
     tmp[3] = 4;
 
+    // Free old array
     free(list2);
 
     list2 = tmp; // set list2 pointer to tmp pointer
 
+    // Print new array
     for (int i = 0; i < 4; i++)
     {
         printf("%i\n", list2[i]);
     }
-    free(list2); 
+    // Free new array
+    free(list2);
     return 0;
 }
