7e54674e345281830bcc6c6ba490de9b89ff4b75
Patrick Bruso <pkbruso@gmail.com>
2022-01-16 21:32:24 +0000

    Sun, Jan 16, 2022, 9:32 PM +00:00

--- week-5/realloc.c
+++ week-5/realloc.c
@@ -5,36 +5,36 @@ int main(void)
 {
     // Example using realloc
     // Dynamically allocate an array of size 3
-    int *list2 = malloc(3 * sizeof(int));
-    if (list2 == NULL)
+    int *list = malloc(3 * sizeof(int));
+    if (list == NULL)
     {
         return 1;
     }
 
     // Assign three numbers to that array
-    list2[0] = 1;
-    list2[1] = 2;
-    list2[2] = 3;
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
 
     // Resize old array to be of size 4
-    int *tmp = realloc(list2, 4 * sizeof(int));
+    int *tmp = realloc(list, 4 * sizeof(int));
     if (tmp == NULL)
     {
-        free(list2);
+        free(list);
         return 1;
     }
 
     // Add fourth number to new array
     tmp[3] = 4;
 
-    list2 = tmp; // set list2 pointer to tmp pointer
+    list = tmp; // set list2 pointer to tmp pointer
 
     // Print new array
     for (int i = 0; i < 4; i++)
     {
-        printf("%i\n", list2[i]);
+        printf("%i\n", list[i]);
     }
     // Free new array
-    free(list2);
+    free(list);
     return 0;
 }
