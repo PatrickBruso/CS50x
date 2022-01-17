1bfd04382ae89df709b987ca23c9961aa5e0fe96
Patrick Bruso <pkbruso@gmail.com>
2022-01-17 18:10:48 +0000

    Mon, Jan 17, 2022, 6:10 PM +00:00

--- week-5/tree.c
+++ week-5/tree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Represents a node
 typedef struct node
 {
     int number;
@@ -9,7 +10,33 @@ typedef struct node
 }
 node;
 
+void free_tree(node *root);
+void print_tree(node *root);
+
 int main(void)
 {
+    // Tree of size 0
+    node *tree = NULL;
 
+    // Add number to list
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+    n->number = 2;
+    n->left = NULL;
+    n->right = NULL;
+    tree = n;
+
+    // Add number to list
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+    n->number = 1;
+    n->left = NULL;
+    n->right = NULL;
+    tree->left = n;
 }
