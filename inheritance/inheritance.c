b24f27c72242fd0e5c6d385b08b9a037e3bd2596
Patrick Bruso <pkbruso@gmail.com>
2022-01-18 00:43:30 +0000

    Tue, Jan 18, 2022, 12:43 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -80,8 +80,13 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
+    if (p == NULL)
+    {
+        return 1;
+    }
 
     // TODO: Free parents recursively
+    free_family()
 
     // TODO: Free child
 
