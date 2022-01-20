4a171633d967e853645734ea42762aef92f70a9d
Patrick Bruso <pkbruso@gmail.com>
2022-01-20 00:12:24 +0000

    Thu, Jan 20, 2022, 12:12 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -60,13 +60,15 @@ bool load(const char *dictionary)
     {
         return false;
     }
+
+    word_count = 0;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    // return word_count variable
+    return word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
