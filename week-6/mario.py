452f8d25726ee09910d8041e1eacd8589f2117f5
Patrick Bruso <pkbruso@gmail.com>
2022-01-21 23:25:22 +0000

    Fri, Jan 21, 2022, 11:25 PM +00:00

--- week-6/mario.py
+++ week-6/mario.py
@@ -1,9 +1,12 @@
 from cs50 import get_int
 
-while True:
-    n = get_int("Height: ")
-    if n > 0:
-        break
+def main():
+    for i in range(n):
+        print("#")
 
-for i in range(n):
-    print("#")
+def get_height():
+    while True:
+        n = get_int("Height: ")
+        if n > 0:
+            break
+    return n
