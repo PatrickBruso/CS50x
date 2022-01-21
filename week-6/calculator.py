cfac92b188e6b916ec9ec3bca92915f2fef8fb63
Patrick Bruso <pkbruso@gmail.com>
2022-01-21 23:03:22 +0000

    Fri, Jan 21, 2022, 11:03 PM +00:00

--- week-6/calculator.py
+++ week-6/calculator.py
@@ -4,10 +4,12 @@ x = get_int("x: ")
 y = get_int("y: ")
 print(x + y)
 
-# without cs50 library (and using exceptions)
+# Without cs50 library (and using exceptions)
 try:
-    x = int(input("x: ")) # cast input string to integer
-except ValueError: # if user does not enter integer
+    # Cast input string to integer
+    x = int(input("x: "))
+    # If user does not enter integer
+except ValueError:
     print("That is not an int!")
     exit()
 try:
