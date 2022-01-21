5bd441b602becba5add84262718639ea468d469c
Patrick Bruso <pkbruso@gmail.com>
2022-01-21 19:00:57 +0000

    Fri, Jan 21, 2022, 7:00 PM +00:00

--- week-6/dictionary.py
+++ week-6/dictionary.py
@@ -1,2 +1,14 @@
-# Implementation of dictionary.c in Python
+# Implementation of pset5 dictionary.c in Python
 
+words = dict()
+
+def check(word):
+    if word in words:
+        return True
+    else:
+        return False
+
+def load(dictionary):
+file = open(dictionary, "r")
+for line in file:
+    
