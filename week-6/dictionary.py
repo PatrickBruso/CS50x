dea01856ae7c0d0b9166f6da5a46d1c34310ef4a
Patrick Bruso <pkbruso@gmail.com>
2022-01-21 19:03:12 +0000

    Fri, Jan 21, 2022, 7:03 PM +00:00

--- week-6/dictionary.py
+++ week-6/dictionary.py
@@ -9,6 +9,15 @@ def check(word):
         return False
 
 def load(dictionary):
-file = open(dictionary, "r")
-for line in file:
-    
+    file = open(dictionary, "r")
+    for line in file:
+        word = line.rstrip()
+        words.add(word)
+    file.close()
+    return True
+
+def size():
+    return len(words)
+
+def unload():
+    return True
