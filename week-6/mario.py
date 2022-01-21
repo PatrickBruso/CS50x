55b6ec60708efa97e81efbf728c0cd857bc9e40e
Patrick Bruso <pkbruso@gmail.com>
2022-01-21 23:29:43 +0000

    Fri, Jan 21, 2022, 11:29 PM +00:00

--- week-6/mario.py
+++ week-6/mario.py
@@ -5,9 +5,12 @@ def main():
 
 def get_height():
     while True:
-        n = int(input("Height: "))
-        if n > 0:
-            break
+        try:
+            n = int(input("Height: "))
+            if n > 0:
+                break
+        except ValueError:
+            print("That's not an integer!")
     return n
 
 main()
