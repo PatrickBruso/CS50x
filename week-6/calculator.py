1a7a22cd37dda31c052c0ab0a48b24c2891b54e5
Patrick Bruso <pkbruso@gmail.com>
2022-01-21 20:55:38 +0000

    Fri, Jan 21, 2022, 8:55 PM +00:00

--- week-6/calculator.py
+++ week-6/calculator.py
@@ -1,5 +1,10 @@
-from cs50 import get_int as gi
+from cs50 import get_int
 
-x = gi("x: ")
-y = gi("y: ")
+x = get_int("x: ")
+y = get_int("y: ")
+print(x + y)
+
+# without cs50 library
+x = int(input("x: ")) # cast input string to integer
+y = int(input("y: "))
 print(x + y)
