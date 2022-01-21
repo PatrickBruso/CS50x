9241bf04717d31b6c4f8bbb4de58438d1015dce9
Patrick Bruso <pkbruso@gmail.com>
2022-01-21 23:17:45 +0000

    Fri, Jan 21, 2022, 11:17 PM +00:00

--- week-6/meow.py
+++ week-6/meow.py
@@ -1,9 +1,11 @@
 def main():
-    for i in range(3):
-        meow()
+    meow(3)
 
 
-def meow():
-    print("meow")
+def meow(n):
+    for i in range(n):
+        print("meow")
 
-main()
+
+if __name__ == "__main__":
+    main()
