41a5cae518a87936caef3d161318084180d642c1
Patrick Bruso <pkbruso@gmail.com>
2022-01-22 00:41:08 +0000

    Sat, Jan 22, 2022, 12:41 AM +00:00

--- week-6/scores.py
+++ week-6/scores.py
@@ -2,7 +2,9 @@ from cs50 import get_int
 
 scores = []
 
-
+for i in range(3):
+    score = get_int("Score: ")
+    scores.append(score)
 
 average = sum(scores) / len(scores)
 print(f"Average: {average}")
