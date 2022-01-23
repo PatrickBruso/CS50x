c4d15d83d0c2ab3d365446bfa9864ea5becda5ec
Patrick Bruso <pkbruso@gmail.com>
2022-01-23 22:17:31 +0000

    Sun, Jan 23, 2022, 10:17 PM +00:00

---
+++ pset6/sentimental-credit/credit.py
@@ -0,0 +1,10 @@
+from cs50 import get_string
+
+def main:
+    number = get_string("Number: ")
+
+    if len(number) is 15:
+        amex(number)
+    elif len(number) is 13:
+        visa(number)
+    elif len(number is 16 and )
