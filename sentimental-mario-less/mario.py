82a18f688cd3ed6f824e16f51f34ac0249228073
Patrick Bruso <pkbruso@gmail.com>
2022-01-23 03:40:35 +0000

    Sun, Jan 23, 2022, 3:40 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,8 +1,9 @@
 from cs50 import get_int
 
+# Obtain height from user
 height = get_int("Height: ")
 if height < 1 or height > 8:
     height = get_int("Height: ")
 
-for i in range(height):
+for i in range(1, height + 1):
     print(" " * (height - i) + "#" * i)
