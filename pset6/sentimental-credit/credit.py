8d15128a2ae1e5302d51bcdcabe788eacddba62f
Patrick Bruso <pkbruso@gmail.com>
2022-01-23 22:31:12 +0000

    Sun, Jan 23, 2022, 10:31 PM +00:00

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -4,10 +4,9 @@ def main():
     number = get_string("Number: ")
 
     # Apply Luhn's Algorithm to determine if valid number
-    sum = 0
+    sum = []
     for i in range(len(number) - 2, -1, -2):
-        sum += (int(number[i]) * 2)
-        print(sum)
+        sum.append((int(number[i]) * 2))
     print(sum)
 
 if __name__ == '__main__':
