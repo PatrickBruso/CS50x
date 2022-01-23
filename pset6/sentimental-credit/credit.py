f6c1fd674f9a9f06987513214d26cfdd795e5385
Patrick Bruso <pkbruso@gmail.com>
2022-01-23 22:29:08 +0000

    Sun, Jan 23, 2022, 10:29 PM +00:00

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -6,7 +6,7 @@ def main():
     # Apply Luhn's Algorithm to determine if valid number
     sum = 0
     for i in range(len(number) - 2, -1, -2):
-        sum += (int(i) * 2)
+        sum += (int(number[i]) * 2)
     print(sum)
 
 if __name__ == '__main__':
