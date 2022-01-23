58a5d80d230da19d81ea2d9088ee9b10468f5049
Patrick Bruso <pkbruso@gmail.com>
2022-01-23 03:42:04 +0000

    Sun, Jan 23, 2022, 3:42 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,8 +2,10 @@ from cs50 import get_int
 
 # Obtain height from user
 height = get_int("Height: ")
+# Re-prompt for height if out of parameters
 if height < 1 or height > 8:
     height = get_int("Height: ")
 
+# Draw half-pyramid
 for i in range(1, height + 1):
     print(" " * (height - i) + "#" * i)
