7dc53e5a4d5b44488a377e1a123ce8ec02594f55
Patrick Bruso <pkbruso@gmail.com>
2022-01-23 22:46:59 +0000

    Sun, Jan 23, 2022, 10:46 PM +00:00

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -4,11 +4,21 @@ def main():
     number = get_string("Number: ")
 
     # Apply Luhn's Algorithm to determine if valid number
+    valid = luhn_algorithm(number)
+    print(valid)
+
+
+def luhn_algorithm(string):
+    # Initialize variables
     products = []
     sum = 0
-    for i in range(len(number) - 2, -1, -2):
-        products.append((int(number[i]) * 2))
 
+    # Iterate from 2nd to last number to beginning by 2
+    for i in range(len(string) - 2, -1, -2):
+        # Multiply each number by 2 and add to list
+        products.append((int(string[i]) * 2))
+
+    # iterate through list and obtain products' digits
     for number in products:
         converted_num = str(number)
         if len(converted_num) == 1:
@@ -16,10 +26,17 @@ def main():
         elif len(converted_num) > 1:
             for num in converted_num:
                 sum += int(num)
-    print(sum)
 
-def luhn_algorithm:
+    # Add sum to digits that weren't multiplied by 2
+    for i in range(len(string) - 1, -1, -2):
+        sum += int(string[i])
 
+    # convert sum to string to determine last digit
+    string_sum = str(sum)
+    if string_sum[-1] == '0':
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
