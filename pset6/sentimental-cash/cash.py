a5a1eaf45a7df13d33e0f79dfae106751e479594
Patrick Bruso <pkbruso@gmail.com>
2022-01-23 21:45:59 +0000

    Sun, Jan 23, 2022, 9:45 PM +00:00

--- pset6/sentimental-cash/cash.py
+++ pset6/sentimental-cash/cash.py
@@ -1,26 +1,30 @@
 from cs50 import get_float
 
 # Obtain change amount (non-negative number)
-change_amount = get_float("Change owed: ")
-while change_amount < 0:
-    change_amount = get_float("Change owed: ")
+change = get_float("Change owed: ")
+while change < 0:
+    change = get_float("Change owed: ")
+
+# Convert to cents
+change = round(change * 100)
 
 # Counter for number of coins given
 coins = 0
 
 # Iterate through change amount and decrease starting with quarters, then dimes, etc.
-while change_amount >= .25:
-    change_amount -= .25
-    coins += 1
-while change_amount >= .10 and change_amount < .25:
-    change_amount -= .10
-    coins += 1
-while change_amount >= .05 and change_amount < .10:
-    change_amount -= .05
-    coins += 1
-while change_amount < .05 and change_amount > 0:
-    change_amount -= .01
-    coins += 1
+while change > 0:
+    while change >= 25:
+        coins += 1
+        change -= 25
+    while change >= 10:
+        coins += 1
+        change -= 10
+    while change >= 5:
+        coins += 1
+        change -= 5
+    while change >= 1:
+        change -= 1
+        coins += 1
 
 # Print number of coins
 print(coins)
