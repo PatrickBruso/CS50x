ec4f06e9f6ee6ee29fe203108ae89a157645f70e
Patrick Bruso <pkbruso@gmail.com>
2022-01-24 02:57:10 +0000

    Mon, Jan 24, 2022, 2:57 AM +00:00

--- pset6/sentimental-readability/readability.py
+++ pset6/sentimental-readability/readability.py
@@ -16,3 +16,14 @@ for characters in text:
         sentences += 1
     if characters == ' ':
         words += 1
+
+l_calc = (letters / words) * 100
+s_calc = (sentences / words) * 100
+
+# Calculate grade index
+index = 0.0588 * l_calc - 0.296 * s_calc - 15.8
+
+print(index)
+
+# call round function on index and assign to integer to remove decimal places
+int grade = round(index)
