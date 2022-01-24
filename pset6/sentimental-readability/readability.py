78659877ab18d0aed61ef4a0da1ad3f0a61ee151
Patrick Bruso <pkbruso@gmail.com>
2022-01-24 02:45:01 +0000

    Mon, Jan 24, 2022, 2:45 AM +00:00

---
+++ pset6/sentimental-readability/readability.py
@@ -0,0 +1,12 @@
+from cs50 import get_string
+
+# Obtain input text
+text = get_string("Text: ")
+
+# Initialize variables
+letters = 0
+words = 1
+sentences = 0
+
+for characters in text:
+    if characters 
