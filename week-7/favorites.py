478d2cdd94f51cf15797e3b3a8023c1945b4ab9c
Patrick Bruso <pkbruso@gmail.com>
2022-01-28 00:45:48 +0000

    Fri, Jan 28, 2022, 12:45 AM +00:00

--- week-7/favorites.py
+++ week-7/favorites.py
@@ -2,8 +2,17 @@ import csv
 
 from cs50 import SQL
 
+# Open db file
 db = SQL("sqlite:///favorites.db")
 
+# Obtain title from user and strip whitespace
 title = input("Title: ").strip()
 
-db.execute("SELECT COUNT(*) FROM favorites WHERE title LIKE ?", title)
+# Execute db query on database to obtain list of rows from favorites where title matches user input
+rows = db.execute("SELECT COUNT(*) AS counter FROM favorites WHERE title LIKE ?", title)
+
+# Take first row
+row = rows[0]
+
+# Print out counter from first row (which is a dictionary)
+print(row["counter"])
