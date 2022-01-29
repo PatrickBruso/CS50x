72561bc06286b090ebcb5595b4de0763bf469b35
Patrick Bruso <pkbruso@gmail.com>
2022-01-29 00:51:00 +0000

    Sat, Jan 29, 2022, 12:51 AM +00:00

--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -1,2 +1,4 @@
 --write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
 
+
+ORDER BY rating DESC LIMIT 5;
