4da8a44d972f6ed93e2b8afbada5b923438a7976
Patrick Bruso <pkbruso@gmail.com>
2022-01-29 21:34:24 +0000

    Sat, Jan 29, 2022, 9:34 PM +00:00

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,2 +1,8 @@
 --write a SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred.
 
+SELECT title FROM movies, stars, people
+WHERE people.id = stars.person_id
+AND stars.movie_id = movies.id
+AND name = "Kevin Bacon";
+
+SELECT * FROM stars WHERE movie_id IN ()
