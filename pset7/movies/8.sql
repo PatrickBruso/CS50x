f8e85d38f848fd8172bb1922b8520711a5a573c2
Patrick Bruso <pkbruso@gmail.com>
2022-01-29 00:25:45 +0000

    Sat, Jan 29, 2022, 12:25 AM +00:00

--- pset7/movies/8.sql
+++ pset7/movies/8.sql
@@ -1,3 +1,3 @@
 --write a SQL query to list the names of all people who starred in Toy Story.
 
-SELECT name FROM people WHERE id = (SELECT name FROM movies WHERE name = "Toy Story");
+SELECT name FROM people WHERE id = (SELECT id FROM movies WHERE title = "Toy Story");
