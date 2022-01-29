af5f943f96c276c4baeebb2e31083bf82b6c2a49
Patrick Bruso <pkbruso@gmail.com>
2022-01-29 21:27:06 +0000

    Sat, Jan 29, 2022, 9:27 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -3,3 +3,6 @@
 SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN ((SELECT id FROM people WHERE name = "Johnny Depp") AND (SELECT id FROM people WHERE name = "Helena Bonham Carter")));
 
 SELECT title FROM movies WHERE id IN ((SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Johnny Depp")) AND (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Helena Bonham Carter")));
+
+SELECT tile FROM movies, stars, people
+WHERE 
