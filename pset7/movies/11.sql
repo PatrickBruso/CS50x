39a250fe2794b2df81e4f7b96f9893eb0fcee72b
Patrick Bruso <pkbruso@gmail.com>
2022-01-29 00:54:26 +0000

    Sat, Jan 29, 2022, 12:54 AM +00:00

--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -1,7 +1,3 @@
 --write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
 
-
-SELECT title FROM movies WHERE id IN ()
-SELECT id IN people WHERE name = "Chadwick Boseman"
-
-ORDER BY rating DESC LIMIT 5;
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id IN people WHERE name = "Chadwick Boseman")) ORDER BY rating DESC LIMIT 5;
