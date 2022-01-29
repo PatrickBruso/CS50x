ecff6c27acbbfe329cfa6895139335c7893bbeb8
Patrick Bruso <pkbruso@gmail.com>
2022-01-29 22:04:03 +0000

    Sat, Jan 29, 2022, 10:04 PM +00:00

--- pset7/movies/7.sql
+++ pset7/movies/7.sql
@@ -1,6 +1,6 @@
 --write a SQL query to list all movies released in 2010 and their ratings, in descending order by rating. For movies with the same rating, order them alphabetically by title.
 
-SELECT title FROM movies, ratings
+SELECT title, rating FROM movies, ratings
 WHERE movies.id = ratings.movie_id
 AND year = 2010
 ORDER BY rating DESC, title;
