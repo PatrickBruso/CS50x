3954018b384d9677672fa74f972d8bb1cf4b6844
Patrick Bruso <pkbruso@gmail.com>
2022-01-29 21:41:54 +0000

    Sat, Jan 29, 2022, 9:41 PM +00:00

--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -2,4 +2,8 @@
 
 SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY (SELECT rating FROM ratings) DESC LIMIT 5;
 
+SELECT title FROM movies, stars, people
+WHERE movie.id = stars.movie_id
+AND 
+
 -- need to use join?
