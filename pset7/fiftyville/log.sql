cb8a0acefcf34cbd28c5861e6ce6e7ccf3722371
Patrick Bruso <pkbruso@gmail.com>
2022-01-30 21:14:45 +0000

    Sun, Jan 30, 2022, 9:14 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -41,14 +41,8 @@ SELECT name, transcript
    AND month = 07
    AND day = 28
    AND name IN (
-        SELECT name
-          FROM people
-         WHERE license_plate
-            IN (
-                SELECT license_plate
-                FROM bakery_security_logs
-                WHERE year = 2021
-                AND month = 07
-                AND day = 28
-                AND hour = 9  AND minute > 13 OR hour = 10 AND minute < 17
-));
+        SELECT name, transcript
+  FROM interviews
+ WHERE year = 2021
+   AND month = 07
+   AND day = 28
