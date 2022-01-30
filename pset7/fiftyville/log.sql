1d5c9a00c6a5bd870b8f374586c37704c21e8ace
Patrick Bruso <pkbruso@gmail.com>
2022-01-30 20:40:30 +0000

    Sun, Jan 30, 2022, 8:40 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -1,3 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Check the crime scene reports table for the date of July 28, 2021 and Humphrey Street
+SELECT description, id
+  FROM crime_scene_reports
+ WHERE year = 2021
+   AND month = 07
+   AND day = 28
+   AND street = "Humphrey Street";
