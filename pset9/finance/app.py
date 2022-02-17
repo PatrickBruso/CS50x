588632abdab716390cfe54c867cdae0d6e0ab066
Patrick Bruso <pkbruso@gmail.com>
2022-02-17 20:47:40 +0000

    Thu, Feb 17, 2022, 8:47 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -104,7 +104,7 @@ def buy():
         db.execute("UPDATE users SET cash=? WHERE id=?", cash-cost, session["user_id"])
 
         # Add purchase to transactions table
-        db.execute("INSERT INTO transactions (symbol, shares, price, date) VALUES (?, ?, ?, ?)", quote['symbol'], request.form.get("shares"), quote['price'], datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+        db.execute("INSERT INTO transactions (symbol, shares, price, date) VALUES (?, ?, ?, ?) WHERE user_id=?", quote['symbol'], request.form.get("shares"), quote['price'], datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), session["user_id"])
 
         # Update user's portfolio with purchase
 
