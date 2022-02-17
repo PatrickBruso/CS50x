e235208ee9a49a2126c4e40a2b084117724277d3
Patrick Bruso <pkbruso@gmail.com>
2022-02-17 20:49:39 +0000

    Thu, Feb 17, 2022, 8:49 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -104,7 +104,7 @@ def buy():
         db.execute("UPDATE users SET cash=? WHERE id=?", cash-cost, session["user_id"])
 
         # Add purchase to transactions table
-        db.execute("INSERT INTO transactions (symbol, shares, price, date) VALUES (?, ?, ?, ?) WHERE user_id=?", quote['symbol'], request.form.get("shares"), quote['price'], datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), session["user_id"])
+        db.execute("INSERT INTO transactions (symbol, shares, price, date) VALUES (?, ?, ?, ?)", quote['symbol'], request.form.get("shares"), quote['price'], datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
 
         # Update user's portfolio with purchase
 
@@ -117,7 +117,7 @@ def buy():
 
         # If already own symbol, update shares amount with new purchase
         else:
-            db.execute("UPDATE portfolio SET shares=? WHERE symbol=? AND user_id=?", shares+int(request.form.get("shares")), quote['symbol'], session["user_id"])
+            db.execute("UPDATE portfolio SET shares=? WHERE symbol=?", shares+int(request.form.get("shares")), quote['symbol'], session["user_id"])
 
         # Render template for index when finished
         return render_template("index.html")
