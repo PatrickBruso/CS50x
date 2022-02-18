8e7f0ecc90f227d42bb1fe49290ca0443a964395
Patrick Bruso <pkbruso@gmail.com>
2022-02-18 21:41:08 +0000

    Fri, Feb 18, 2022, 9:41 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -111,7 +111,7 @@ def buy():
         db.execute("UPDATE users SET cash=? WHERE id=?", cash-cost, session["user_id"])
 
         # Add purchase to transactions table
-        db.execute("INSERT or IGNORE INTO transactions (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", session["user_id"], quote['symbol'], request.form.get("shares"), quote['price'], datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+        db.execute("INSERT INTO transactions (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", session["user_id"], quote['symbol'], request.form.get("shares"), quote['price'], datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
 
         # Update user's portfolio with purchase
 
@@ -305,7 +305,7 @@ def sell():
         db.execute("UPDATE users SET cash=? WHERE id=?", cash+proceeds, session["user_id"])
 
         # Add sale to transactions table
-        db.execute("INSERT or IGNORE INTO transactions (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", session["user_id"], quote['symbol'], -int((request.form.get("shares"))), quote['price'], datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+        db.execute("INSERT INTO transactions (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", session["user_id"], quote['symbol'], -int((request.form.get("shares"))), quote['price'], datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
 
         # Update shares amount with sale
         db.execute("UPDATE portfolio SET shares=? WHERE symbol=?", shares[0]["shares"]-int(request.form.get("shares")), quote['symbol'])
