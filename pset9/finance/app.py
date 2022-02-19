8ccf518cd048ceea6d944201d20d49a2cf1f74a0
Patrick Bruso <pkbruso@gmail.com>
2022-02-19 04:36:39 +0000

    Sat, Feb 19, 2022, 4:36 AM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -255,7 +255,7 @@ def register():
         # Store new user in db
         try:
             new_user = db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), pwhash)
-            return redirect('/buy')
+            return redirect('/')
         except:
             return apology("username is already registered", 400)
 
