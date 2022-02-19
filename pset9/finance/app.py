956c64f9cb05fc8dd2cf0cb5e7e5e71eca1c4916
Patrick Bruso <pkbruso@gmail.com>
2022-02-19 04:40:37 +0000

    Sat, Feb 19, 2022, 4:40 AM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -184,7 +184,7 @@ def login():
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
-        return redirect("/buy")
+        return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
