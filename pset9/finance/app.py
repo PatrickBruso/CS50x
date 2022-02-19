9205b63e013270b29d7ab036b717944b67aa6b44
Patrick Bruso <pkbruso@gmail.com>
2022-02-19 04:54:16 +0000

    Sat, Feb 19, 2022, 4:54 AM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -50,7 +50,7 @@ def index():
 
     # Check to make sure user has made a purchase
     if not portfolio:
-        return apology("You have no purchases")
+        return apology("You have no purchases", 200)
 
     # Obtain user's cash
     obtain_cash = db.execute("SELECT cash FROM users where id=?", session["user_id"])
