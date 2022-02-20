6b2e2eeca03f5afee894001f17b221f6a31e4e2a
Patrick Bruso <pkbruso@gmail.com>
2022-02-20 04:45:03 +0000

    Sun, Feb 20, 2022, 4:45 AM +00:00

--- project/pixelator.py
+++ project/pixelator.py
@@ -7,31 +7,15 @@ def main(file_location, palette_name):
 
     # Resize image to 1/4 of original
     with Image.open(file_location) as image:
-        (width, height) = (image.width // 4, image.height // 4)
-        image_resized = image.resize((width, height))
+        image_resized = image.resize((image.width // 4, image.height // 4))
 
-        pixel_image = pixelate(image_resized, palette)
+        pixel_image = Image.new('RGB', (image.width, image.height))
 
-
-def pixelate(image, palette):
-    """
-    Function that replaces pixel from target image with pixel from a set color palette
-    that closest resembles target  pixel and returns a copy of the image
-    """
-
-    # Get width and height
-    width, height = image.size
-    image_copy = Image.new('RGB', (width, height))
-
-    for x in range(width):
-        for y in range(height):
-            old_pixel = image.getpixel((x, y))
-            new_pixel = color_picker(old_pixel, palette)
-            image_copy.putpixel((x, y), new_pixel)
-
-    # Should just move this code to main function
-
-    return image_copy
+        for x in range(image.width):
+            for y in range(image.height):
+                old_pixel = image.getpixel((x, y))
+                new_pixel = color_picker(old_pixel, palette)
+                pixel_image.putpixel((x, y), new_pixel)
 
 
 if __name__ == "__main__":
