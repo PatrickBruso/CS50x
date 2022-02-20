e0f922ddd89bfb4e5ce5938466e310bf00134cce
Patrick Bruso <pkbruso@gmail.com>
2022-02-20 04:09:46 +0000

    Sun, Feb 20, 2022, 4:09 AM +00:00

--- project/pixelator.py
+++ project/pixelator.py
@@ -13,5 +13,28 @@ def main(file_location, palette_name):
         pixel_image = pixelate(image_resized, palette)
 
 
+def pixelate(image, palette):
+    """
+    Function that replaces pixel from target image with pixel from a set color palette
+    that closest resembles target  pixel and returns a copy of the image
+    """
+
+    # Get width and height
+    width, height = image.size
+    image_copy = Image.new('RGB', (width, height))
+
+    for new_pixel in image_copy:
+        x = new_pixel.x
+        y = new_pixel.y
+        old_pixel = image.get_pixel(x, y)
+        palette_color = color_picker(old_pixel, palette)
+        new_pixel.red = palette_color[0]
+        new_pixel.green = palette_color[1]
+        new_pixel.blue = palette_color[2]
+        image_copy.set_pixel(x, y, new_pixel)
+
+    return image_copy
+
+
 if __name__ == "__main__":
     main("static/images/landscape.jpg", "ammo8.png")
